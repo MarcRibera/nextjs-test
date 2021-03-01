e5de141ec2d7104cbdb42c3e59ab791089823112
MarcR <mribera@thesocialcoin.com>
2021-03-01 16:47:53 +0100

    add barchart into dashboard page to visualize nasa data

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,4 +1,5 @@
-import { LineChart, Line , CartesianGrid, Legend, XAxis, YAxis, Tooltip, ResponsiveContainer} from 'recharts';
+import { LineChart, BarChart, Bar, Line , CartesianGrid, Legend, XAxis, YAxis, Tooltip, ResponsiveContainer} from 'recharts';
+import { useState, useEffect } from 'react'
 
 const dashboard = () => {
   const data = [
@@ -45,28 +46,86 @@ const dashboard = () => {
     amt: 2100,
   },
 ];
+
+ const [asteroidsData, setAsteroidsData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const res = await fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-10&api_key=i9kr4lTxq7qRJjn032dF9yhmgmQ7nrr8giXfq9Dy')
+    const data = await res.json();
+    buildData(data)
+  }
+
+  const buildData = (data) => {
+    const nearEarthObjects = data.near_earth_objects;
+    let chartData = [];
+
+    for (let date in nearEarthObjects ) {
+      chartData.push({date:date, value: nearEarthObjects[date].length})
+    }
+    console.log('chartData',chartData);
+    setAsteroidsData(chartData)
+
+  }
+
+  const wrapperStyle = {
+     display: 'block',
+     marginBottom: '70px',
+     marginLeft: 'auto',
+     marginRight: 'auto',
+     width: `70%`,
+     height: '320px',
+  }
+
+  const chartMargins = {
+    top: 5,
+    right: 30,
+    left: 20,
+    bottom: 5,
+  }
+
   return(
     <>
       <h1>dashboard</h1>
+      <p>Some charts made by recharts.js</p>
+
+      <h3>Fake Data</h3>
+      <div style={wrapperStyle}>
+       <ResponsiveContainer width="100%" height="100%">
         <LineChart
-          width={700}
-          height={300}
           data={data}
-          margin={{
-              top: 25,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}>
+          margin={chartMargins}>
           <Line type="monotone" dataKey="uv" stroke="#8884d8" />
           <Line type="monotone" dataKey="pv" stroke="#82ca9d" activeDot={{ r: 8 }} />
           <Line type="monotone" dataKey="amt" stroke="#ff0000"/>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis />
-          <Legend />
+          <Legend verticalAlign="top" align="right" height={36}/>
           <Tooltip />
         </LineChart>
+        </ResponsiveContainer>
+      </div>
+
+      <h3>Retrieve a list of Asteroids based on their closest approach date to Earth.</h3>
+      <div style={wrapperStyle}>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={700}
+            height={300}
+            data={asteroidsData}
+            margin={chartMargins}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <Tooltip />
+            <Legend verticalAlign="top" align="right" height={36}/>
+            <Bar dataKey="value" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+
     </>
   )
 }
